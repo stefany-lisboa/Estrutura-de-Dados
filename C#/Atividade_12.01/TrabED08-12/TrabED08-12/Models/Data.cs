ed98c85a47ceb779ea64b04930bbff1e956bb11a
Stefany Lisboa <stefany22lisboa@gmail.com>
2021-01-20 22:10:53 -0300

    Upload

---
+++ C#/Atividade_12.01/TrabED08-12/TrabED08-12/Models/Data.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrabED08_12.Models
+{
+    class Data
+    {
+        private int dia;
+        private int mes;
+        private int ano;
+
+
+        public Data()
+        {
+            dia = 1;
+            mes = 1;
+            ano = 1900;
+        }
+
+        public void setData(int d, int m, int a)
+        {
+            this.dia = d;
+            this.mes = m;
+            this.ano = a;
+        }
+
+        public override string ToString()
+        {
+            DateTime a = new DateTime(ano, mes, dia);
+            string sa = a.ToString("dd/MM/yyyy");
+            return sa;
+        }
+
+        public int Dia
+        {
+            get { return dia; }
+            set { dia = value; }
+        }
+
+        public int Mes
+        {
+            get { return mes; }
+            set { mes = value; }
+        }
+
+        public int Ano
+        {
+            get { return ano; }
+            set { ano = value; }
+        }
+
+    }
+}
---
+++ C#/Atividade_12.01/TrabED08-12c/TrabED08-12c/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrabED08_12c.Models;
+
+namespace TrabED08_12c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Contatos contatos = new Contatos(10);
+            int opc;
+            Console.WriteLine("0. Sair");
+            Console.WriteLine("1. Adicionar contato");
+            Console.WriteLine("2. Pesquisar contato");
+            Console.WriteLine("3. Alterar contato");
+            Console.WriteLine("4. Remover contato");
+            Console.WriteLine("5. Listar contatos");
+            Console.WriteLine("Escolha uma opção: ");
+            opc = int.Parse(Console.ReadLine());
+
+            while (opc != 0)
+            {
+                if (opc == 1)
+                {
+                    Contato contato = new Contato();
+
+                    Console.WriteLine("Digite o nome: ");
+                    string nome = Console.ReadLine();
+
+                    Console.WriteLine("Digite o email: ");
+                    string email = Console.ReadLine();
+
+                    Console.WriteLine("Digite o telefone: ");
+                    string telefone = Console.ReadLine();
+
+                    Console.WriteLine("Digite o dia do nascimento: ");
+                    int dia = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite o mes do nascimento: ");
+                    int mes = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite o ano do nascimento: ");
+                    int ano = int.Parse(Console.ReadLine());
+
+                    Data data = new Data();
+                    data.setData(dia, mes, ano);
+
+                    contato.Nome = nome;
+                    contato.Email = email;
+                    contato.Telefone = telefone;
+                    contato.DtNasc = data;
+
+                    contatos.adicionar(contato);
+                }
+                if (opc == 2)
+                {
+                    Contato contato = new Contato();
+
+                    Console.WriteLine("Digite o nome: ");
+                    string nome = Console.ReadLine();
+                    contato.Nome = nome;
+
+                    contato = contatos.pesquisar(contato);
+
+                    if (contato.Nome != "")
+                    {
+                        Console.WriteLine(contato.ToString());
+                    }
+                    else
+                    {
+                        Console.WriteLine("Não encontrado!");
+                    }
+
+                }
+                if (opc == 3)
+                {
+                    Contato contato = new Contato();
+
+                    Console.WriteLine("Digite o nome do contato existente: ");
+                    string nome = Console.ReadLine();
+
+                    Console.WriteLine("Digite o email: ");
+                    string email = Console.ReadLine();
+
+                    Console.WriteLine("Digite o telefone: ");
+                    string telefone = Console.ReadLine();
+
+                    Console.WriteLine("Digite o dia do nascimento: ");
+                    int dia = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite o mes do nascimento: ");
+                    int mes = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Digite o ano do nascimento: ");
+                    int ano = int.Parse(Console.ReadLine());
+
+                    Data data = new Data();
+                    data.setData(dia, mes, ano);
+
+                    contato.Nome = nome;
+                    contato.Email = email;
+                    contato.Telefone = telefone;
+                    contato.DtNasc = data;
+
+                    contatos.alterar(contato);
+                }
+                if (opc == 4)
+                {
+                    Contato contato = new Contato();
+
+                    Console.WriteLine("Digite o nome: ");
+                    string nome = Console.ReadLine();
+                    contato.Nome = nome;
+
+                    contatos.remover(contato);
+                }
+                if (opc == 5)
+                {
+                    foreach (Contato c in contatos.Agenda)
+                    {
+                        if (c.Nome != "")
+                        {
+                            Console.WriteLine(c.ToString());
+                        }
+                    }
+                }
+
+                Console.WriteLine("0. Sair");
+                Console.WriteLine("1. Adicionar contato");
+                Console.WriteLine("2. Pesquisar contato");
+                Console.WriteLine("3. Alterar contato");
+                Console.WriteLine("4. Remover contato");
+                Console.WriteLine("5. Listar contatos");
+                Console.WriteLine("Escolha uma opção: ");
+                opc = int.Parse(Console.ReadLine());
+            }
+
+        }
+    }
+}
